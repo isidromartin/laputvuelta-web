dce07dae36142299028811e2d8e56a793fb5cc01
Isidro <i.martin@pristok.com>
2026-01-12 22:50:46 +0100

    robots b2b

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/", disallow: "/studio" }],
+    rules: [{ userAgent: "*", allow: "/", disallow: ["/studio", "/b2b"] }],
     sitemap: "https://laputvuelta.com/sitemap.xml",
   };
 }
