a7daf2d00a8d7df323b442d08e8b163124ae620b
Isidro <i.martin@pristok.com>
2026-01-27 00:01:33 +0100

    pasarela fv not ok fix2

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,18 @@ const nextConfig: NextConfig = {
             key: "Cross-Origin-Opener-Policy",
             value: "same-origin-allow-popups",
           },
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.botpress.cloud",
+              "connect-src 'self' https://cdn.botpress.cloud wss://cdn.botpress.cloud",
+              "img-src 'self' data: blob: https:",
+              "style-src 'self' 'unsafe-inline' https:",
+              "font-src 'self' data: https:",
+              "frame-src 'self' https://web.fourvenues.com https://www.fourvenues.com",
+            ].join("; "),
+          },
         ],
       },
     ];
